4438975e17a2d9fe6f2e4c185ced9fd80bb96850
philbenz@gmail.com <philbenz@Morrhysseys-MacBook-Air.local>
2016-07-25 16:53:07 -0600

    all 6 passed pushing to the clone on my git

--- src/example.js
+++ src/example.js
@@ -1,3 +1,54 @@
 module.exports = {
-  // your code here
+  simplePromise: simplePromise,
+  add10Promise: add10Promise,
+  reject: reject,
+  sum50: sum50
+
+}
+
+function simplePromise(bool) {
+
+  return new Promise(function(resolve, reject) {
+    if( bool ) {
+      resolve('OK');
+    } else {
+      reject('BAD');
+    }
+  }). catch(function(err){
+    return err;
+  });
+}
+
+function add10Promise(num) {
+
+  return new Promise(function(resolve, reject) {
+    if (num != 10) {
+    if (isNaN(num)){
+      num = 0;
+    }
+    resolve(num + 10);
+  } else {
+    reject(10);
+  }
+  });
+}
+
+function reject(num) {
+  return new Promise(function(resolve, reject) {
+    if ( isNan(num)) {
+      resolve(num);
+    } else {
+      reject(num);
+    }
+  }
+
+)
+}
+
+function sum50() {
+
+  return new Promise(function(resolve, reject) {
+    resolve(50);
+  });
+
 }
